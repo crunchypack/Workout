27bc8ede54ee86a5f023b8dec49042284b27446d
crunchypack <35976650+crunchypack@users.noreply.github.com>
2021-10-15 09:16:40 -0400

    Update method ex (#8)

--- Workout/Models/ExerciseMethods.cs
+++ Workout/Models/ExerciseMethods.cs
@@ -72,5 +72,41 @@ namespace Workout.Models
                 conn.Close();
             }
         }
+        public int UpdateExercise(Exercise ex, out string error)
+        {
+            SqlConnection conn = new()
+            {
+                ConnectionString = "Data Source=(localDB)\\MSSQLLocalDB;Initial Catalog=Workout;Integrated Security=True"
+            };
+            String query = "Update tbl_exersice SET ex_name = @name, ex_category = @cat, ex_weight = @weight, ex_muscle = @muscle, ex_workout = @workout WHERE ex_id = @id";
+            SqlCommand comm = new(query, conn);
+
+            comm.Parameters.Add("name", System.Data.SqlDbType.VarChar, 20).Value = ex.Name;
+            comm.Parameters.Add("cat", System.Data.SqlDbType.VarChar, 20).Value = ex.Category;
+            comm.Parameters.Add("weight", System.Data.SqlDbType.Decimal).Value = ex.Weight;
+            comm.Parameters.Add("muscle", System.Data.SqlDbType.VarChar, 20).Value = ex.Muscle;
+            comm.Parameters.Add("workout", System.Data.SqlDbType.Int).Value = ex.Workout.WorkoutId;
+            comm.Parameters.Add("id", System.Data.SqlDbType.Int).Value = ex.ExerciseId;
+
+            error = "";
+            int res = 0;
+            try
+            {
+                conn.Open();
+                res = comm.ExecuteNonQuery();
+                if (res != 1) error = "Unable to remove exercice";
+                return res;
+
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+                return res;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
     }
 }
